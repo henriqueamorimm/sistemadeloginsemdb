5704fdbcda6723adb51786a2d570060f64c69c38
Aligg <114702742+Aligg0001@users.noreply.github.com>
2022-12-29 02:30:06 -0300

    Add files via upload
    Para executar:
    1. Abra todas as pastas até chegar no Program.cs
    2. Execute o comando "cmd" no diretório da pasta.
    3. Use o comando dotnet run.
    4. Enjoy! :)

---
+++ MeuProjetoV3/MeuProjetoV3/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Timers;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Inicio:
+            Console.Clear();
+            Console.Title = "Sistema de login e criar conta.";
+            string user = "Aligg";
+            string senha = "henrique@lindo";
+            Console.WriteLine("Deseja fazer log-in ou criar uma conta?");
+            Console.WriteLine("");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("1. Log-in");
+            Console.WriteLine("2. Criar Conta");
+            Console.Write("Sua escolha:"); int escolha = int.Parse(Console.ReadLine());
+            if (escolha == 1)
+            {
+                Console.Clear();
+                goto Login;
+
+            }
+            if (escolha == 2)
+            {
+                Console.Clear();
+                goto CriarConta;
+
+            }
+        Login:
+            Console.WriteLine("Olá usuário, faça seu Log-in!");
+            Console.Write("User:"); string user_RES = Console.ReadLine();
+            Console.Write("Senha:"); string senha_RES = Console.ReadLine();
+            if (user_RES == user && senha_RES == senha)
+            {
+                Console.Clear();
+                Console.WriteLine($"Sucesso! Você logou como {user}!");
+                Console.WriteLine();
+                Console.WriteLine();
+                Thread.Sleep(5000);
+                Console.WriteLine("Retornando para o sistema em 10 segundos...");
+                Thread.Sleep(10000);
+                goto Inicio;
+            }
+        CriarConta:
+            Console.Clear();
+            Console.WriteLine("Preencha as informações abaixo:");
+            Console.Write("Seu nome de usuário:"); user = Console.ReadLine();
+            Console.Write("Senha:"); senha = Console.ReadLine();
+            Console.Clear();
+            goto Login;
+
+
+
+        }
+
+
+
+    }
+}
